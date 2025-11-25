b597b85071b008c17ba9541fbb25ef65faf39b53
samuel castro <samuelcr11@outlook.com>
2025-11-24 20:20:24 -0500

    actualizacion para deplegar

--- config/settings.py
+++ config/settings.py
@@ -91,19 +91,17 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+import dj_database_url
+import os
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'agroapp_db',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '3306',
-        'options': {
-            'init_command': "SET sql mode='STRICT_TRANS_TABLES'",
-        }
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=False
+    )
 }
+
 
 
 # Password validation
